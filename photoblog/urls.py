3b109ec10082346a4c30d6826121ab5addfcbcc7
Rolf Erik Lekang <relekang@gmail.com>
2012-11-18 15:40:13 +0100

    add a view to view one photo

--- photoblog/urls.py
+++ photoblog/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('photoblog.views',
     url(r'^$', 'index', name='photoblog_index'),
+    url(r'^(?P<id>\d+)/$', 'photo', name='photoblog_view_photo'),
 )
--- photoblog/views.py
+++ photoblog/views.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 from django.conf import settings
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from photoblog.models import Photo
 
 def index (request):
@@ -16,3 +16,14 @@ def index (request):
         'date_as_title': getattr(settings, 'PHOTOBLOG_DATE_AS_TITLE', False)
     })
 
+def photo (request, id):
+    photo = get_object_or_404(
+        Photo.objects.prefetch_related('tags').select_related('category', 'location'),
+        pk=id,
+        date_published__lte=datetime.now()
+    )
+
+    return render(request, 'photoblog/index.html', {
+        'photo': photo,
+        'date_as_title': getattr(settings, 'PHOTOBLOG_DATE_AS_TITLE', False)
+    })
